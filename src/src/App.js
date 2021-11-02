e4c313d61ee3b6471893e91ba401378aeef8fd20
IvlevaAnna <annaiva19@mail.ru>
2021-11-03 01:08:25 +0300

    FE-002 Created Board page

--- src/src/App.js
+++ src/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {SignIn} from "./components/SignIn/SignIn";
+import {BoardPage} from "./components/BoardPage/BoardPage";
+
 
 function App() {
   return (
       <div className='App'>
-        <SignIn/>
+          <BoardPage />
       </div>
   );
 }
---
+++ src/src/components/BoardPage/BoardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Header} from "../Header/Header";
+import {Board} from "../Board/Board";
+
+export const BoardPage = () => {
+    return (
+        <div>
+            <Header />
+            <Board />
+        </div>
+    )
+}
