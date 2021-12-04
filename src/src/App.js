cc34ff412b54a33863083360b392e9b7638d7ad5
ivlevok <anna.ivleva@firstlinesoftware.com>
2021-12-04 17:33:19 +0300

    FE-004 Added router with history

--- src/src/App.js
+++ src/src/App.js
@@ -1,13 +1,25 @@
+import {
+    Route,
+    Switch,
+    Redirect,
+    withRouter
+} from "react-router-dom"
 import './App.css';
 import {SignIn} from "./components/SignIn/SignIn";
 import { BoardPage } from "./components/BoardPage/BoardPage";
 
-function App() {
-  return (
-    <div className='App'>
-      <BoardPage />
-    </div>
-  );
+function App(props) {
+    const { history } = props.history
+
+    return (
+        <div className='App'>
+            <Switch>
+                <Route history={history} path='/home' component={SignIn} />
+                <Route history={history} path='/board' component={BoardPage} />
+                <Redirect from='/' to='/board'/>
+            </Switch>
+        </div>
+    );
 }
 
-export default App;
+export default withRouter(App);
