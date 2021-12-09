7e85c9898aa9d2f345a483cc90b5532578dfe11b
IvlevaAnna <annaiva19@mail.ru>
2021-12-10 01:24:44 +0300

    FE-006 Added priority selector

--- src/src/components/AddCardForm/AddCardForm.jsx
+++ src/src/components/AddCardForm/AddCardForm.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import style from './AddCardForm.module.css'
 import { showCardForm, hideCardForm } from '../../appSlice';
+import {PrioritySelector} from "../PrioritySelector/PrioritySelector";
 
 export function AddCardForm() {
     // const [isShown, setIsShown] = useState(true);
@@ -25,7 +26,7 @@ export function AddCardForm() {
             <div className={style.formContainer} onClick={disableClose}>
                 <form className={style.flex}>
                     <div className={style.formLeft}>
-                        <div className={style.inputLeft}>Tag for select</div>
+                        <PrioritySelector />
                         <input type="date" className={style.inputLeft}></input>
                         <input placeholder="Add location" className={style.inputLeft}></input>
                         <input type="file" placeholder="Add file" className={style.inputLeft}></input>
