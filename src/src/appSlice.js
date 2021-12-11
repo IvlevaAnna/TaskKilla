4a910ee37e7e35fd91d1311f886fa61b506186ff
ivlevok <anna.ivleva@firstlinesoftware.com>
2021-12-11 22:38:08 +0300

    FE-007 Added get post requests

--- src/src/appSlice.js
+++ src/src/appSlice.js
@@ -8,6 +8,7 @@ export const appSlice = createSlice({
     isMapShown: false,
     userLat: "",
     userLng: "",
+    taskList: [],
   },
   reducers: {
     showCardForm: (state) => {
@@ -31,9 +32,12 @@ export const appSlice = createSlice({
     setUserLng: (state, action) => {
       state.userLng = action.payload;
     },
+    setTaskList: (state, action) => {
+      state.taskList = action.payload;
+    },
   },
 })
 
-export const { showCardForm, hideCardForm, setCategory, showMap, hideMap, setUserLat, setUserLng } = appSlice.actions
+export const { showCardForm, hideCardForm, setCategory, showMap, hideMap, setUserLat, setUserLng, setTaskList } = appSlice.actions
 
 export default appSlice.reducer
