91a27c0dcc6d48029e81fb106b053de7cf183200
Tima <02.timoha.0@gmail.com>
2021-12-11 21:22:17 +0300

    add redux

--- src/src/appSlice.js
+++ src/src/appSlice.js
@@ -4,21 +4,36 @@ export const appSlice = createSlice({
   name: 'app',
   initialState: {
     isCardFormShown: false,
-    category: ""
+    category: "",
+    isMapShown: false,
+    userLat: "",
+    userLng: "",
   },
   reducers: {
     showCardForm: (state) => {
       state.isCardFormShown = true;
     },
     hideCardForm: (state) => {
-        state.isCardFormShown = false;
+      state.isCardFormShown = false;
     },
     setCategory: (state, action) => {
-        state.category = action.payload;
-    }
+      state.category = action.payload;
+    },
+    showMap: (state) => {
+      state.isMapShown = true;
+    },
+    hideMap: (state) => {
+      state.isMapShown = false;
+    },
+    setUserLat: (state, action) => {
+      state.userLat = action.payload;
+    },
+    setUserLng: (state, action) => {
+      state.userLng = action.payload;
+    },
   },
 })
 
-export const { showCardForm, hideCardForm, setCategory } = appSlice.actions
+export const { showCardForm, hideCardForm, setCategory, showMap, hideMap, setUserLat, setUserLng } = appSlice.actions
 
 export default appSlice.reducer
