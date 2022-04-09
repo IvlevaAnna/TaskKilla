8609390eaceaab2ae555945c927fc5815d711575
AllStars123 <purnov.nikita@yandex.ru>
2022-04-10 02:03:14 +0300

    add new integration tests

--- server/Taskilla/tests.py
+++ server/Taskilla/tests.py
@@ -6,6 +6,9 @@ from django.contrib.auth import get_user_model
 from .views import MainView
 from .models import Cards
 import datetime
+from unittest.mock import patch
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test.client import Client
 
 User = get_user_model()
 
@@ -216,4 +219,107 @@ class TestMain(APITestCase):
         response = self.client.post(f'{self.url}', self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+
+class IntegrationsTest(TestCase):
+    def setUp(self) -> None:
+        self.image = (
+            b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04"
+            b"\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02"
+            b"\x02\x4c\x01\x00\x3b"
+        )
+        self.url = '/api/main_page/'
+        self.google_url = '/'
+        self.error_url = '/fkdlv/'
+
+        self.data = {
+            'title': 'Mock',
+            'description': 'Mock',
+            'status': 'Mock',
+            'priority': 'Mock',
+            'location': 'Mock',
+            # 'image': self.image,
+            'deadline': datetime.datetime.now().date(),
+            # 'user': 'admin'
+        }
+        self.data_patch = {
+            'status': 'to_Do',
+        }
+
+    @patch("django.core.files.storage.FileSystemStorage._save")
+    def test_all_actions_with_card(self, mock_save):
+        mock_save.return_value = "test.gif"
+        test_gif = SimpleUploadedFile(
+            name="test.gif", content=self.image
+        )
+        # card = Cards.objects.create(**self.data)
+        factory = APIRequestFactory()
+        request = factory.post(self.url, self.data, format='json')
+        view = MainView.as_view({'post': 'create'})
+        response_post = view(request)
+
+        request = factory.get(self.url)
+        view = MainView.as_view({'get': 'list'})
+        response_get = view(request)
+
+        # request = factory.delete(f"{self.url}{card.id}/")
+        # view = MainView.as_view({'delete': 'destroy'})
+        # response_delete = view(request)
+        #
+        # request = factory.put(f"{self.url}{card.id}/")
+        # view = MainView.as_view({'put': 'update'})
+        # response_put = view(request)
+
+        # self.assertEqual(response_put.status_code, status.HTTP_200_OK)
+        # self.assertEqual(response_delete.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_post.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_get.status_code, status.HTTP_200_OK)
+
+        print(test_gif)
+
+    def test_authorization_google_acc(self):
+        client = Client()
+        factory = APIRequestFactory()
+        response = client.post('/google', {'username': 'test@gmail.com', 'password': 'Q!werty15'})
+        request = factory.get(self.url)
+        view = MainView.as_view({'get': 'list'})
+        response_get_cards = view(request)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response_get_cards.status_code, status.HTTP_200_OK)
+
+    def test_coincidence_location(self):
+        card = Cards.objects.create(**self.data)
+        card_ = Cards.objects.get(id=card.id)
+        self.assertEqual(card_.location, self.data.get('location'))
+
+    def test_change_account(self):
+        client = Client()
+        factory = APIRequestFactory()
+        client.post('/google', {'username': 'test@gmail.com', 'password': 'Q!werty15'})
+        request = factory.get(self.url)
+        view = MainView.as_view({'get': 'list'})
+        response_get_cards = view(request)
+        data_first_acc = response_get_cards
+        client.logout()
+
+        client.post('/google', {'username': 'nikita@gmail.com', 'password': 'kmdkk123%4'})
+        request = factory.get(self.url)
+        view = MainView.as_view({'get': 'list'})
+        response_get_cards = view(request)
+        data_second_acc = response_get_cards
+        self.assertEqual(f"{data_first_acc}", f"{data_second_acc}")
+
+    def test_error_page(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.error_url)
+        url = request.get_full_path()
+        self.assertEqual(url, self.error_url)
+
+    def test_change_status(self):
+        client = APIClient()
+        card = Cards.objects.create(**self.data)
+        status_before = card.status
+        different_statuses = status_before != card.status
+        response = client.patch(f'{self.url}{card.id}/', self.data_patch)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(different_statuses)
 
